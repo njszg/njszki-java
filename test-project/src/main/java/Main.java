eb96028a65def24af777a215abef5396f94977f7
turizsolt <turi.zsolt.m@gmail.com>
2016-09-22 20:23:45 +0200

    First java codes.

---
+++ test-project/src/main/java/Main.java
@@ -0,0 +1,22 @@
+package main.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public int getRandomNumber(){
+        return 4;   // chosen by fair dice roll.
+                    // guaranteed to be random
+    }
+
+    public boolean isEven(int i) {
+        return (i%2 == 0);
+    }
+
+    public int[] solveQuadratic(int i, int i1, int i2) {
+        int[] a = new int[0];
+        return a;
+    }
+}
