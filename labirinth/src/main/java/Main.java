9e24f487fabdf66e18ffdda3ca38a51afdf69744
turizsolt <turi.zsolt.m@gmail.com>
2016-12-15 19:49:22 +0100

    labirinth

---
+++ labirinth/src/main/java/Main.java
@@ -0,0 +1,30 @@
+package main.java;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        /*int sizeX = 60;
+        int sizeY = 12;
+        char[][] grid = new char[sizeY][sizeX];
+
+        for(int i=0;i<sizeY;i++){
+            for(int j=0;j<sizeX;j++){
+                grid[i][j] = (i*j%2 == 1)?'#':' ';
+            }
+        }
+
+        for(int i=0;i<sizeY;i++){
+            for(int j=0;j<sizeX;j++){
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }*/
+
+        while(true){
+            int a = System.in.read();
+            System.out.write(a);
+        }
+    }
+}
